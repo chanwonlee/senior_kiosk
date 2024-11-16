5e6e2c58ade76390fc27665ccbeca2b50a15bfc6
chanwon <clw8679@naver.com>
2024-11-16 21:54:56 +0900

    v5-final

--- src/app/item/page.tsx
+++ src/app/item/page.tsx
@@ -1,13 +1,23 @@
-"use client"
+"use client";
 
-import {useRouter, useSearchParams} from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
-import React, {Suspense, useState} from "react";
+import React, { Suspense, useState, useEffect } from "react";
 
 export default function Page() {
+  useEffect(() => {
+    // 페이지 로드 시 스크롤을 비활성화
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      // 컴포넌트가 unmount될 때 원래 상태로 복원
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
-    <Suspense fallback={<div>로딩 중...</div>}>
-      <SearchParamsComponent/>
+    <Suspense>
+      <SearchParamsComponent />
     </Suspense>
   );
 }
@@ -28,7 +38,9 @@ function SearchParamsComponent() {
     code: string;
   }
 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("cart") || "[]"));
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("cart") || "[]")
+  );
 
   const plus = () => {
     setQuantity((prevQuantity) => Math.min(prevQuantity + 1, 10));
@@ -39,18 +51,17 @@ function SearchParamsComponent() {
   };
 
   const addToCart = () => {
-    // 현재 항목이 장바구니에 이미 있는지 확인
-    const existingItemIndex: number = items.findIndex((item: CartItem) => item.code === code);
+    const existingItemIndex: number = items.findIndex(
+      (item: CartItem) => item.code === code
+    );
 
     if (existingItemIndex !== -1) {
-      // 기존 항목이 있으면 수량을 추가
       const updatedItems = [...items];
       updatedItems[existingItemIndex].quantity += quantity;
       setItems(updatedItems);
       localStorage.setItem("cart", JSON.stringify(updatedItems));
     } else {
-      // 새 항목이면 장바구니에 추가
-      const newItem = {name: itemName, price: parseInt(price || "0"), quantity, code};
+      const newItem = { name: itemName, price: parseInt(price || "0"), quantity, code };
       const updatedItems = [...items, newItem];
       setItems(updatedItems);
       localStorage.setItem("cart", JSON.stringify(updatedItems));
@@ -58,58 +69,58 @@ function SearchParamsComponent() {
     router.push("/cart");
   };
 
-  return <div className="flex flex-col min-h-screen">
-    <div className="h-[60vh] bg-gradient-to-b from-red-400 to-white flex flex-col items-center justify-center">
-      <div className="size-[32vh] bg-white rounded-full flex items-center justify-center shadow-lg mb-4">
-        <Image
-          className="object-cover"
-          width={150}
-          height={150}
-          src={`/png/items/${code}.png`}
-          alt="item"
-        />
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="w-full bg-gradient-to-b from-red-400 to-white flex flex-col items-center justify-center">
+        <div className="w-[32vh] h-[32vh] mt-10 bg-white rounded-full flex items-center justify-center shadow-lg mb-4">
+          <Image
+            className="object-cover w-full h-full"
+            width={150}
+            height={150}
+            src={`/png/items/${code}.png`}
+            alt="item"
+          />
+        </div>
+        <div className="text-center pt-10">
+          <p className="text-3xl font-bold text-gray-700">{itemName}</p>
+          <p className="text-4xl font-semibold text-gray-900 pt-4">{price}</p>
+        </div>
       </div>
-      <div className="text-center pt-10">
-        <p className="text-3xl font-bold text-gray-700">{itemName}</p>
-        <p className="text-4xl font-semibold text-gray-900 pt-4">{price}</p>
-      </div>
-    </div>
 
-    <div className="flex flex-col flex-grow items-center">
-      <div className="flex flex-col mt-8">
-        <div
-          className="border-4 rounded-2xl border-red-600 text-4xl p-4 flex flex-row items-center justify-between"
-        >
-          <div className="flex flex-row items-center">
-            <div className="ml-2 pr-2 whitespace-nowrap text-4xl text-black font-['Paperlogy']">수량:</div>
-            <div className="text-black text-4xl font-['Paperlogy']">{quantity}</div>
+      <div className="flex flex-col flex-grow items-center">
+        <div className="flex flex-col mt-8 w-full px-4">
+          <div className="border-4 rounded-2xl border-red-600 text-4xl p-4 flex flex-row items-center justify-between">
+            <div className="flex flex-row items-center">
+              <div className="ml-2 pr-2 whitespace-nowrap text-4xl text-black font-['Paperlogy']">수량:</div>
+              <div className="text-black text-4xl font-['Paperlogy']">{quantity}</div>
+            </div>
+            <div className="flex flex-row justify-end">
+              <Image
+                className="size-[4vh] mr-1"
+                src="/png/item/minus.png"
+                alt={"minus"}
+                width={50}
+                height={50}
+                onClick={minus}
+              />
+              <Image
+                className="size-[4vh] ml-1"
+                src="/png/item/plus.png"
+                alt={"plus"}
+                width={50}
+                height={50}
+                onClick={plus}
+              />
+            </div>
           </div>
-          <div className="flex flex-row justify-end">
-            <Image
-              className="size-[4vh] mr-1"
-              src="/png/item/minus.png"
-              alt={"minus"}
-              width={50}
-              height={50}
-              onClick={minus}
-            />
-            <Image
-              className="size-[4vh] ml-1"
-              src="/png/item/plus.png"
-              alt={"plus"}
-              width={50}
-              height={50}
-              onClick={plus}
-            />
+          <div
+            className="bg-red-500 border-4 rounded-2xl border-red-600 text-4xl text-white text-center p-4 justify-between mt-4 font-['Paperlogy']"
+            onClick={addToCart}
+          >
+            장바구니에 넣기
           </div>
-        </div>
-        <div
-          className="bg-red-500 border-4 rounded-2xl border-red-600 text-4xl text-white text-center p-4 justify-between mt-4 font-['Paperlogy']"
-          onClick={addToCart}
-        >
-          장바구니에 넣기
         </div>
       </div>
     </div>
-  </div>
+  );
 }
